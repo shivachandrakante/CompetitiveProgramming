0e6467fa013a140aaf0c742a8b1b5baac116ff85
ShivaChandra <k.s9908725092@gmail.com>
2020-09-02 19:21:04 +0530

    added Subarray Sums Divisible by K solution

---
+++ ba.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopne("output.txt","w",stdout);
+#endif
+
+	//code
+	
+}
