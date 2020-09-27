54cd5190950bc861f0d832dcf1815fea260b5c76
ShivaChandra <k.s9908725092@gmail.com>
2020-09-27 21:56:07 +0530

    adding rotate-image

---
+++ rotate-image.cpp
@@ -0,0 +1,37 @@
+/*
+https://leetcode.com/problems/rotate-image/
+*/
+
+
+
+
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n=matrix.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                matrix[i][j]=matrix[i][j]+matrix[j][i];
+                matrix[j][i]=matrix[i][j]-matrix[j][i];
+                matrix[i][j]=matrix[i][j]-matrix[j][i];
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n/2;j++)
+            {
+                matrix[i][j]=matrix[i][j]+matrix[i][n-j-1];
+                matrix[i][n-j-1]=matrix[i][j]-matrix[i][n-j-1];
+                matrix[i][j]=matrix[i][j]-matrix[i][n-j-1];    
+            }
+        }
+        return;
+        
+    }
+};
