d06eb383fff5a01926ff1c057807653cabbcc731
ShivaChandra <k.s9908725092@gmail.com>
2020-09-28 20:48:53 +0530

    adding kth-largest-element-in-an-array

---
+++ kth-largest-element-in-an-array.cpp
@@ -0,0 +1,14 @@
+/*
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+*/
+
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-k];
+    }
+};
